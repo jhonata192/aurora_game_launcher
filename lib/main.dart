16eee5b8356a8b5488dce762202a1fa2fe6a24ef
jhonata192 <fjhonata14@gmail.com>
2024-08-16 13:24:16 -0300

    implementação do instalador de pacotes do android
    foi implementado o instalador de pacotes + ajustado as permissões no manifest.xml tanto na versão debug quanto na release
    
    Co-Authored-By: Juan Mathews Rebello Santos <azurejoga@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:android_package_installer/android_package_installer.dart';
 import 'dart:io';
 
 void main() => runApp(GameLauncher());
@@ -22,23 +24,66 @@ class LauncherHome extends StatefulWidget {
 class _LauncherHomeState extends State<LauncherHome> {
   Dio dio = Dio();
   String gameUrl = 'https://masterjf-solucoes.com.br/downloads/android/fernandes-image-describer.apk';
-  String downloadPath = '/storage/emulated/0/Download/game.apk';
+  String? downloadPath;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeDownloadPath();
+  }
+
+  Future<void> _initializeDownloadPath() async {
+    final directory = await getExternalStorageDirectory();
+    if (directory != null) {
+      setState(() {
+        downloadPath = '${directory.path}/game.apk';
+      });
+    } else {
+      // Trate o caso em que o diretório não pode ser obtido
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Não foi possível acessar o diretório de armazenamento.')),
+      );
+    }
+  }
 
   Future<void> downloadAndInstall() async {
-    try {
-      // Baixa o arquivo APK
-      await dio.download(gameUrl, downloadPath, onReceiveProgress: (received, total) {
-        if (total != -1) {
-          print((received / total * 100).toStringAsFixed(0) + "%");
-        }
-      });
+    if (await Permission.storage.request().isGranted &&
+        await Permission.requestInstallPackages.request().isGranted) {
+      if (downloadPath != null) {
+        try {
+          // Baixa o arquivo APK
+          await dio.download(gameUrl, downloadPath!, onReceiveProgress: (received, total) {
+            if (total != -1) {
+              print((received / total * 100).toStringAsFixed(0) + "%");
+            }
+          });
 
-      // Após o download, abre o instalador do APK
-      OpenFile.open(downloadPath);
-    } catch (e) {
-      print('Erro ao baixar o jogo: $e');
+          // Após o download, abre o instalador do APK
+          final result = await AndroidPackageInstaller.installApk(apkFilePath: downloadPath!);
+
+          if (result != null) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Instalação do jogo iniciada: ${PackageInstallerStatus.byCode(result).name}')),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Falha ao iniciar a instalação.')),
+            );
+          }
+        } catch (e) {
+          print('Erro ao baixar o jogo: $e');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Falha ao baixar o jogo.')),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Caminho de download não definido.')),
+        );
+      }
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Falha ao baixar o jogo.')),
+        SnackBar(content: Text('Permissão de armazenamento ou instalação de pacotes negada.')),
       );
     }
   }
@@ -49,7 +94,7 @@ class _LauncherHomeState extends State<LauncherHome> {
       appBar: AppBar(title: Text('Game Launcher')),
       body: Center(
         child: ElevatedButton(
-          onPressed: downloadAndInstall,
+          onPressed: downloadPath == null ? null : downloadAndInstall,
           child: Text('Baixar e Instalar Jogo'),
         ),
       ),
